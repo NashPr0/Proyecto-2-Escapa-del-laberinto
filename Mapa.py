93d275ab5556302e4cfc7ebb7ea619a64a6805c9
luisdi2520 <luismurillo.ldm25@gmail.com>
2025-11-16 16:56:10 -0600

    prueba de commit luis diego

--- Mapa.py
+++ Mapa.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
 
     print("\nLeyenda:")
     print("  # = muro")
-    print("    = camino")
+    print("  C  = camino")
     print("  L = liana")
     print("  T = tunel")
 
