da354c72f82801debe71c933c93808ed46308acf
NashPr0 <anthony.m.l16022005@gmail.com>
2025-11-22 17:41:54 -0600

    constantes Generales para generacion de Mapa

--- Interfaz_completa.py
+++ Interfaz_completa.py
@@ -5,3 +5,16 @@ import random
 import time
 import os
 from collections import deque
+
+# CONSTANTES GENERALES
+
+CAMINO = 0
+MURO = 1
+LIANA = 2
+TUNEL = 3
+SALIDA = 4
+
+TAM_CELDA = 32 
+
+ANCHO_MAPA = 23
+ALTO_MAPA = 15
