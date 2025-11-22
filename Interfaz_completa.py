eb496a5d7e39c39cad5489cae715e7db3cc1d4b1
NashPr0 <anthony.m.l16022005@gmail.com>
2025-11-22 17:52:57 -0600

    Creacion clase Mapa

--- Interfaz_completa.py
+++ Interfaz_completa.py
@@ -18,3 +18,22 @@ TAM_CELDA = 32
 
 ANCHO_MAPA = 23
 ALTO_MAPA = 15
+
+class Mapa:
+    def __init__(self, ancho, alto):
+        self.ancho = ancho
+        self.alto = alto
+        self.m = [[MURO for _ in range(ancho)] for _ in range(alto)]
+
+        self.entrada = (alto - 2, 1)
+
+        self.salidas = [
+            (1, ancho // 2),
+            (alto - 2, ancho // 2),
+            (alto // 2, 1),
+            (alto // 2, ancho - 2),
+        ]
+
+        self._generar_laberinto()
+        self._garantizar_camino_valido()
+        self._colocar_terrenos_especiales()
