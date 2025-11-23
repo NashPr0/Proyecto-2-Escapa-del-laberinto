6dd82e79d1e157fa030a589619847f8f1725ccd6
luisdi2520 <luismurillo.ldm25@gmail.com>
2025-11-22 20:39:33 -0600

    movimiento y regeneración del jugador

--- LuisDiego.py
+++ LuisDiego.py
@@ -7,6 +7,20 @@ class jugador:
     def posicion(self):
         return(self.fila, self.col)
     
+    def mover(self, df, dc, mapa, corriendo=False):
+        nf = self.fila + df
+        nc = self.col + dc
+        if mapa.es_valido_jugador(nf, nc):
+            self.fila = nf
+            self.col = nc
+            costo = COSTO_CORRER if corriendo else 1
+            self.energia = max(0, self.energia - costo)
+            return True
+        return False
+
+    def regenerar(self):
+        if self.energia < MAX_ENERGIA:
+            self.energia = min(MAX_ENERGIA, self.energia + REGEN_ENERGIA)
     
 
     
